8fa4073051417bc18f549c25105cd07fb8629a59
Sergey Holin <holin_sergey@yahoo.com>
2022-06-30 19:24:57 -0700

    3rd commit: fixed timer and timer penalty

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,16 +7,14 @@
 
 
 
-// TIMER------------------------------------
-var timerEl = document.getElementById('countdown');
-var buttonElOne = document.querySelector('#Answer1');
-var buttonElTwo = document.querySelector('#Answer2');
-var buttonElThree = document.querySelector('#Answer3');
-var buttonElFour = document.querySelector('#Answer4');
+
+
+
 
 // Timer----------------------------------------------------------------------
-function countdown() {
+    var timerEl = document.getElementById('countdown');
     var timeLeft = 60; 
+
     var timeInterval = setInterval(function () {
      if (timeLeft > 1) {
      timerEl.textContent = 'Timer: ' + timeLeft;
@@ -24,145 +22,120 @@ function countdown() {
      } else {
      timerEl.textContent = '0';
      clearInterval(timeInterval);
-      }
+    }
     }, 1000);
-  }
-// timer end------------------------------------------------------------------------------------
-countdown();
-// time penalty--------------------------------------------------------------------------------
-//   var timePenalty = buttonEl.addEventListener("click", function() {
-//     if (timeLeft > 1) {
-//         // Set the `textContent` of `timerEl` to show the remaining seconds
-//         timerEl.textContent = 'Timer: ' + timeLeft;
-//         // Decrement `timeLeft` by 1
-//         timeLeft-=5;
-//       } else {
-//         // Once `timeLeft` gets to 0, set `timerEl` to an empty string
-//         timerEl.textContent = '0';
-//         // Use `clearInterval()` to stop the timer
-//         clearInterval(timeInterval);
-//       }
-//     }, 1000);
-  //time penalty end---------------------------------------------------------------------------- 
-    
+// Timer Penalty Function-------------------------------------------------------------
+    function timePenalty () {
+    if (timeLeft > 1) {
+    timerEl.textContent = 'Timer: ' + timeLeft;
+    timeLeft-=5;
+    } else {
+    timerEl.textContent = '0';
+    clearInterval(timeInterval);
+    }1000;
+    };
+// timer end------------------------------------------------------------------------------------   
 //Populate form function---------------------------------------------------------------------
 function populate () {
-    var questionOrder = ["Question 1", "Question 2"]
-    var questionText = ["Question 1 bla bla", "Question 2 bla bla"]
-    var questionOneAnswers = ["a1","a2","a3","a4"]
-    var h1 = document.querySelector('h1');
-    var p = document.querySelector("#p");
-    var one = document.querySelector("#answer1");
-    var two = document.querySelector("#answer2");
-    var three = document.querySelector("#answer3");
-    var four = document.querySelector("#answer4");
+    var questionOrder = ["Question 1"]
+    var questionText = ["Question 1 bla bla bla"]
+    var questionAnswers = ["a1","a2","a3","a4"]
+    // Randomizing questions in the array
+    let questionAnswersRandomized = questionAnswers
+    .map(value => ({ value, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map(({ value }) => value)
+
+    var questionH1 = document.querySelector('h1');
+    var questionP = document.querySelector("#p");
+    var answerOne = document.querySelector("#answer1");
+    var answerTwo = document.querySelector("#answer2");
+    var answerThree = document.querySelector("#answer3");
+    var answerFour = document.querySelector("#answer4");
      // Adds text from 1 entry of question array
-    h1.innerText = questionOrder[0];
+    questionH1.innerText = questionOrder[0];
     // adds text 1 1st entry from question text array
-    p.innerText = questionText[0];
+    questionP.innerText = questionText[0];
     // adds answers 1-4
-    one.innerText = questionOneAnswers[0];
-    two.innerText = questionOneAnswers[1];
-    three.innerText = questionOneAnswers[2];
-    four.innerText = questionOneAnswers[3];
+    answerOne.innerText = questionAnswersRandomized[0];
+    answerTwo.innerText = questionAnswersRandomized[1];
+    answerThree.innerText = questionAnswersRandomized[2];
+    answerFour.innerText = questionAnswersRandomized[3];
 };
 // populate form function end----------------------------------------------------------------------
 populate();
 // User input Function-----------------------------------------------------------------------------
 function userInput () {
-var correctAnswers = ["a1"]
-// BUTTON 1
-var buttonElOne = document.querySelector("#answer1");
-buttonElOne.addEventListener("click", function() {
- // on click of button 1 it equals entry one
-// ($(buttonElOne).text() will read it text value as text
-if ($(buttonElOne).text() === correctAnswers[0]) {
+var answerKey = ["a1"]
+var buttonOne = document.querySelector('#Answer1');
+var buttonTwo = document.querySelector('#Answer2');
+var buttonThree = document.querySelector('#Answer3');
+var buttonFour = document.querySelector('#Answer4');
+// BUTTON 1----------------------------------------------------------------------------------------
+var buttonOne = document.querySelector("#answer1");
+buttonOne.addEventListener("click", function() {
+// ($(buttonOne).text() will read it text value as text and will validate it against answer key
+if ($(buttonOne).text() === answerKey[0]) {
 alert("Correct");
 } else {
-alert("Incorrect");
+// alert("Incorrect");
+timePenalty ();
 };
  });
- console.log(buttonElOne);
- console.log(correctAnswers[0]);
-//  Button 2
-var buttonElTwo = document.querySelector("#answer2");
-buttonElTwo.addEventListener("click", function() {
- // on click of button 1 it equals entry one
-// ($(buttonElOne).text() will read it text value as text
-if ($(buttonElTwo).text() === correctAnswers[0]) {
+//  console.log(buttonOne);
+//  console.log(answerKey[0]);
+//  BUTTON 2------------------------------------------------------------------------------------------
+var buttonTwo = document.querySelector("#answer2");
+buttonTwo.addEventListener("click", function() {
+// ($(buttonTwo).text() will read it text value as text and will validate it against answer key
+if ($(buttonTwo).text() === answerKey[0]) {
 alert("Correct");
 } else {
-alert("Incorrect");
+// alert("Incorrect")
+timePenalty ();
 };
  });
- console.log(buttonElTwo);
- console.log(correctAnswers[0]);
-//  Button 3
-var buttonElThree = document.querySelector("#answer3");
-buttonElThree.addEventListener("click", function() {
- // on click of button 1 it equals entry one
-// ($(buttonElOne).text() will read it text value as text
-if ($(buttonElThree).text() === correctAnswers[0]) {
+//  console.log(buttonTwo);
+//  console.log(answerKey[0]);
+//  BUTTON 3--------------------------------------------------------------------------------------------
+var buttonThree = document.querySelector("#answer3");
+buttonThree.addEventListener("click", function() {
+// ($(buttonThree).text() will read it text value as text and will validate it against answer key
+if ($(buttonThree).text() === answerKey[0]) {
 alert("Correct");
 } else {
-alert("Incorrect");
+// alert("Incorrect");
+timePenalty ();
 };
  });
- console.log(buttonElThree);
- console.log(correctAnswers[0]);
- //  Button 4
-var buttonElFour = document.querySelector("#answer4");
-buttonElFour.addEventListener("click", function() {
- // on click of button 1 it equals entry one
-// ($(buttonElOne).text() will read it text value as text
-if ($(buttonElFour).text() === correctAnswers[0]) {
+//  console.log(buttonThree);
+//  console.log(answerKey[0]);
+ //  BUTTON 4----------------------------------------------------------------------------------------------
+var buttonFour = document.querySelector("#answer4");
+buttonFour.addEventListener("click", function() {
+// ($(buttonFour).text() will read it text value as text and will validate it against answer key
+if ($(buttonFour).text() === answerKey[0]) {
 alert("Correct");
 } else {
-alert("Incorrect");
+// alert("Incorrect");
+timePenalty ();
 };
  });
- console.log(buttonElThree);
- console.log(correctAnswers[0]);
+//  console.log(buttonThree);
+//  console.log(answerKey[0]);
 };
 // User input Function end-----------------------------------------------------------------------------
 userInput ();
 
 
+
+
+
+
+ 
  
 
 
-
- 
- 
-
-// // var two = document.querySelector("#answer2");
-// var buttonElTwo = document.querySelector("#answer2");
-// buttonElTwo.addEventListener("click", function() {
-//     // on click of button 1 it equals entry one
-//     buttonElTwo = two;
-//  });
-
-//  if (buttonElOne === correctAnswers[0]) {
-//     alert("Correct");
-//  } else {
-//     alert("Incorrect");
-//  };
-
-
-
-
-
-
-
-
-
-
-// Now i need function to valida correct answer against incorrect answers
-var correctAnswers = ["a1","a5","a9","a14"]
-
-function validate () {
-
-}
 // -------------------------------------------------------------------------------------------------
 
 
